455cf0a3b11a0d1b52674d00ae4d5ea2b637bbf7
Louise Dalton <louisedalton22@gmail.com>
2020-11-12 20:31:37 +0000

    initial commit

---
+++ Rcode.R
@@ -0,0 +1,32 @@
+
+
+library(tidyverse)
+data<- rbind(benign, malware)
+memory.limit(size=56000) #www.researchgate.net/post/How_to_solve_Error_cannot_allocate_vector_of_size_12_Gb_in_R
+data<- rbind(benign, malware) #combine both dataframes
+
+#remove a single  variable from the workspace
+rm(TestingData)
+rm(TrainingData)
+rm(CleanedTrainingData)
+rm(spl)
+
+##########reducing 
+#use the malware dataset to generate a matrix of permissions requested by malicious apps
+
+#### install needed packages
+install.packages("caTools")
+library(caTools)
+install.packages("RWeka")
+library(RWeka)
+
+#split data into training and testing subsets
+spl=sample.split(data$Mal_Ben, SplitRatio = 0.7)
+TrainingData = subset(data, spl==TRUE)
+TestingData = subset(data, spl==FALSE)
+
+#change Mal_Ben variable to string
+CleanedTrainingData <- lapply(TrainingData$Mal_Ben, as.factor)
+
+#create a J48 decision tree
+DecisionTree <- J48(`Mal_Ben`~., data=TrainingData)
